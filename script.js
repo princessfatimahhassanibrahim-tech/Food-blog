03b757864a89a728e97dbe61507d2454e11c97bc
princessfatimahhassanibrahim-tech <princessfatimahhassanibrahim@gmail.com>
2026-06-30 13:47:34 +0100

    Fixed smooth scrolling layout and added section IDs

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-const SubcribeBtn = document.getElementid("subcribeBtn");
-subcribeBtn.addEventListener("click", Function(){
-    alert("Thank You For Subcribing To Foodie Haven!");
-});
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
 
-    const readButtons = document.querySelectorAll("card button");
-
-    readButton.forEach(function(button){
-        button.addEventListener("click", function(){
-            alert("Recipe coming soon! 🍽️");
-        });
-    });
-
-    document.querySelectorAll('nav a').forEach(anchor =>{
-        anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behaviour;"smooth"
-        });
-    });
+menuToggle.addEventListener('click', () => {
+  // This toggles a class named 'show' on your menu
+  navLinks.classList.toggle('show');
 });
